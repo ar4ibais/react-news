6bde7f7818f88eaf0acad58669dcc747698122ba
Arthur <ak.hutov@yandex.ru>
2025-01-30 01:35:46 +0300

    добавил компонент скелетона для главной новости и мелких новостей

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -3,16 +3,21 @@ import NewsBanner from "../../components/NewsBanner";
 import styles from "./styles.module.css";
 import { getNews } from "../../api/apiNews";
 import NewsList from "../../components/NewsList";
+import Skeleton from "../../components/Skeleton";
 
 
 const Main = () => {
   const [news, setNews] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true)
         const response = await getNews()
         setNews(response.news)
+        console.log(news[0]);
+        setIsLoading(false)
       } catch (error) {
         console.log(error);
       }
@@ -21,8 +26,8 @@ const Main = () => {
   }, [])
   return (
     <main className={styles.main}>
-        {news.length > 0 && <NewsBanner item={news[0]} />}
-        <NewsList news={news.slice(1)} />
+        {news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton count={1} type="banner" />}
+        {!isLoading ? <NewsList news={news.slice(1)} /> : <Skeleton count={10} type="item" />}
     </main>
   )
 }
